841fe42b2864bca2352e26453c2aa9ccaca0bfc3
U180709053 <baristetik@posta.mu.edu.tr>
2024-04-09 15:04:07 +0300

    main class of snake game

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Old School Snake")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:  # Detect Collision with food
+        food.respawn()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect Collision with walls
+
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect Collision with tail
+    # If head collides with any segments of the body call game over
+    for segment in snake.turtles[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
