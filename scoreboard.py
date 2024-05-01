81060718400ef06fbb826bb45e37a94715b7790a
U180709053 <baristetik@posta.mu.edu.tr>
2024-05-01 19:31:11 +0300

    updated to hold the highscore even after closing the game

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("highscore.txt") as highscore:
+            self.high_score = int(highscore.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -17,7 +18,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -25,8 +26,10 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def reset(self):
-        if self.score > self.highscore:
-            self.highscore = self.score
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore.txt",mode="w") as highscore:
+                highscore.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
